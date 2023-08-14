aa6300f8ffd49a1e057aef6318d40e35e3af141f
Tomek <klingaroo@gmail.com>
2023-08-14 14:21:29 -0400

    check column names in downloaded data

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -1,10 +1,16 @@
 from contextlib import nullcontext as does_not_raise
 
-import pytest
+import csv
 
-from src.downloader import get_metadata
+from src.downloader import get_metadata, COL_NAMES
 
 
 def test_get_metadata():
     with does_not_raise():
         get_metadata()
+
+        with open("out/metadata.csv", "r") as csvfile:
+            reader = csv.reader(csvfile)
+            header = next(reader)
+
+    assert header == COL_NAMES
