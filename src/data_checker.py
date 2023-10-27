82672c800974383a0f00bb0306a1adeb2bc79a84
Tomek <klingaroo@gmail.com>
2023-10-27 09:38:51 -0400

    add barcode dup verification

---
+++ src/data_checker.py
@@ -0,0 +1,15 @@
+from src.producer import _barcodes2list
+from src.reader import read_data
+
+
+def verify_barcodes():
+    unique_barcodes = set()
+
+    data = read_data()
+    for item in data:
+        barcodes_lst = _barcodes2list(item.barcode)
+        for barcode in barcodes_lst:
+            if barcode in unique_barcodes:
+                print(f"Found duplicate barcode: {barcode} for {item.t245}")
+            else:
+                unique_barcodes.add(barcode)
